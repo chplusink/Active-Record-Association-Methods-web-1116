937cea3fa3ea91ca21dd2b67e8080e419272f0ac
Vic Lam <lam.vic.k@gmail.com>
2016-12-13 17:56:28 -0500

    Drake coded this

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,9 +3,16 @@ class Song < ActiveRecord::Base
   belongs_to :genre
 
   def get_genre_name
+    self.genre.name
   end
 
   def drake_made_this
     # when this method is called it should assign the song's artist to Drake
+    # binding.pry
+    drake = Artist.find_by(name: 'Drake')
+    if drake == nil
+      drake = Artist.new(name: 'Drake')
+    end
+    self.artist = drake
   end
 end
